39cb38fa2c1cfaa83282e0cf3571d6d8cfd91778
jiyachawla <jiyachawla15@gmail.com>
2026-06-15 13:56:17 +0530

    commit

---
+++ session2.py
@@ -0,0 +1,29 @@
+# session=2
+# tasks Q1
+
+# item_price=250
+# delivery_fee=30
+# cupon_discount=20
+# print("item price:" , item_price)
+# print("delivery fee:" , delivery_fee)
+# print("cupon discount:" , cupon_discount)
+
+# Q2
+# price=1200
+# if price>1000:
+    # print("eligible for free delivery")
+# else:
+    # print("delivery charges apply")
+
+# Q3
+# ""
+# this section checks whether a flipkart product is eligible for free delivery
+# ""
+
+# Q4
+# team_name="RCB"
+# runs_scored=185
+# overs_played=20
+# print("Team name:" , team_name)
+# print("Runs scored:", runs_scored)
+# print("Overs played:", overs_played)
