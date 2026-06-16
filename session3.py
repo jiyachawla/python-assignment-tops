217e0a028ca7d8a95288e7d75d26ad89d3877b49
jiyachawla <jiyachawla15@gmail.com>
2026-06-16 12:53:06 +0530

    commit

---
+++ session3.py
@@ -0,0 +1,33 @@
+# # Q1
+# age=17
+# height=165.5
+# name="jiya"
+# has_spotify=True
+# print(age,type(age))
+# print(height,type(height))
+# print(name,type(name))
+# print(has_spotify,type(has_spotify))
+
+# Q2
+# def total_cart_amount(prices):
+    # total=0
+
+    # for price in prices:
+        # total+=float(price)
+    # return total
+# prices=["1199.99","49", "350.75"]
+# print(total_cart_amount(prices))
+
+#Q3
+# score=input("enter cricket score:")
+# score=int(score)
+# if score>=50:
+    # print("half century")
+# else:
+    # print("keep going")
+
+# Q4
+# is_premium="true"
+# is_premium=(is_premium=="True")
+# print(is_premium)
+# print(type(is_premium))
