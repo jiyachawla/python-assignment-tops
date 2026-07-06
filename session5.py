bcd938e3c0981d42b519d704210685c67abd0c02
jiyachawla <jiyachawla15@gmail.com>
2026-07-06 14:20:02 +0530

    Create session5.py
    commit

---
+++ session5.py
@@ -0,0 +1,34 @@
+# Q1
+# play_list_ids=[101,102,103,104,105]
+# print("PlayList IDs", play_list_ids)
+
+# Q2
+# playlist_ids=[101,102,103,104,105]
+# playlist_ids.append(106)
+# playlist_ids.extend([107,108])
+# print("updated playlist:",playlist_ids)
+
+# Q3
+# playlist_ids=[101,102,103,104,105,106,107,108]
+# removed=playlist_ids.pop()
+# print("removed id:",removed)
+# print("remaining playlist:",playlist_ids)
+
+# Q4
+# insta_filters=("clarendon","juno","lark","valencia")
+# print(insta_filters)
+# insta_filters[1]="gingham"===(error)
+# reason:tuples are immutable,so their values cannot be changed after creation
+
+# Q5
+# list of recent zomato orders ids
+# zomato_orders=[5001,5002,5003]
+# tuple if ipl tean names
+# ipl_teams=(
+#     "mumbai indians",
+    # "chennai super king",
+    # "royal challengers bengaluru",
+    # "rajasthan royals"
+# )
+# print("zomato orders:",zomato_orders)
+# print("ipl teams:",ipl_teams)1
