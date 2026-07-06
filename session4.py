ec5fd700e4a6087e35b8a6a1dcad00a1a4c9d578
jiyachawla <jiyachawla15@gmail.com>
2026-07-06 14:16:51 +0530

    commit

---
+++ session4.py
@@ -0,0 +1,29 @@
+# text="FLIPKART-SALE2024"
+# text=text.lower()
+# text=text.replace("-"," ")
+# print(text)
+
+# Q2
+# product_name="OnePlus Nord-CE 3"
+# clean_name=product_name.strip()
+# clean_name=clean_name.upper()
+# clean_name=clean_name.replace("-"," ")
+# print(clean_name)
+
+# Q3
+# def split_product_code(product_code):
+    # return product_code.split("-")
+# code="ZOMATO-FOOD-PD-2024"
+# parts=split_product_code(code)
+# print(parts)
+
+# Q4 
+# text="Spotify Premium Offer"
+# word=text[8:15]
+# print(word)
+
+# Q5
+# product="Myntra Shirt"
+# price=799.5
+# message=f"Deal: {product} is available at {price:2f} only!"
+# print(message)
