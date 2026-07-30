1cc71b3fe539bca0ae097702ada8049f7d07e040
jiyachawla <jiyachawla15@gmail.com>
2026-07-30 23:49:03 +0530

    Create session7.py
    commit

---
+++ session7.py
@@ -0,0 +1,38 @@
+# task 1
+# time = int(input("Enter your Spotify listening time (in minutes): "))
+# if time > 120:
+#     print("You are a true music fan!")
+# else:
+#     print("Keep listening!")
+
+# task 2
+# amount = float(input("Enter your Zomato order amount: "))
+# if amount > 300:
+#     print("Eligible for free delivery.")
+# else:
+#     print("Delivery charges apply.")
+
+# task 3
+# total = float(input("Enter your Flipkart cart total: "))
+# if total > 2000:
+#     discount = total * 0.10
+#     print("You get a 10% discount.")
+#     print("Discount Amount:", discount)
+#     print("Final Amount:", total - discount)
+# elif total > 1000:
+#     discount = total * 0.05
+#     print("You get a 5% discount.")
+#     print("Discount Amount:", discount)
+#     print("Final Amount:", total - discount)
+# else:
+#     print("No discount available.")
+#     print("Final Amount:", total)
+
+# task 4
+# points = int(input("Enter your IPL fantasy team points: "))
+# if points > 800:
+#     print("Champion")
+# elif points >= 500:
+#     print("Top Performer")
+# else:
+#     print("Keep Trying")
