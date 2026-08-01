8a8c69824f03b9c8bc0fbbbf15ce28b14db91591
jiyachawla <jiyachawla15@gmail.com>
2026-08-01 12:58:20 +0530

    Create session1.py
    commit

---
+++ session1.py
@@ -0,0 +1,29 @@
+# task1
+# Download Anaconda from the official website.
+# Install it by following the installation wizard.
+# Open Anaconda Navigator after installation.
+# Click the Launch button under Jupyter Notebook.
+# Jupyter Notebook will open in your web browser.
+
+#  task2
+# Open Jupyter Notebook.
+# Click New → Python 3 (or Python 3.x).
+# Save the notebook as insta_intro.ipynb.
+# In the first code cell, write a Python program using the print() function to display your name and your favorite app.
+# Run the cell by pressing Shift + Enter.
+
+# task3
+# Click the + (Plus) button to create a new code cell.
+# Write a Python statement that prints the message "Welcome to Data Science with Python!".
+# Execute the cell using Shift + Enter.
+# The output will appear directly below the code cell.
+
+# task4
+# Open a text editor such as VS Code, Notepad, or PyCharm.
+# Create a new file and save it as hello_app.py.
+# Write a Python program that prints "Hello from .py file!".
+# Save the file.
+# Open the terminal or Command Prompt.
+# Navigate to the folder where the file is saved.
+# Run the command:
+# The terminal will display the message "Hello from .py file!".
